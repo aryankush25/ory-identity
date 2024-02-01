5417a6d73b06707daa78d2e962cc72bc0c43b0ea
Aryan Agarwal <aryankush025@gmail.com>
2024-02-01 20:07:28 +0530

    Refactor registration page to use dynamic form rendering

--- ory-ui-nextjs/src/pages/registration.tsx
+++ ory-ui-nextjs/src/pages/registration.tsx
@@ -1,21 +1,75 @@
-import { basePath, getUrlForFlow, isQuerySet, ory } from "@/services/ory";
 import { GetServerSideProps } from "next";
+import { useCallback } from "react";
+import { basePath, getUrlForFlow, isQuerySet, ory } from "@/services/ory";
+import { UiNode, UiNodeInputAttributes } from "@ory/client";
+import {
+  filterNodesByGroups,
+  isUiNodeInputAttributes,
+} from "@ory/integrations/ui";
 
 interface RegistrationProps {
   flow?: any;
-  error?: any;
 }
 
-const Registration = () => {
-  return <div>Your registration form goes here</div>;
+const Registration = ({ flow }: RegistrationProps) => {
+  const mapUINode = useCallback((node: UiNode, key: number) => {
+    if (isUiNodeInputAttributes(node.attributes)) {
+      const attrs = node.attributes as UiNodeInputAttributes;
+      const nodeType = attrs.type;
+
+      switch (nodeType) {
+        case "button":
+        case "submit":
+          return (
+            <button
+              className="w-full px-3 py-2 mb-2 text-sm font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              title="Submit"
+              type={attrs.type as "submit" | "reset" | "button" | undefined}
+              name={attrs.name}
+              value={attrs.value}
+            >
+              Submit
+            </button>
+          );
+        default:
+          return (
+            <input
+              className="w-full px-3 py-2 mb-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              title="Input field"
+              placeholder={"Enter value for " + attrs.name}
+              name={attrs.name}
+              type={attrs.type}
+              autoComplete={
+                attrs.autocomplete || attrs.name === "identifier"
+                  ? "username"
+                  : ""
+              }
+              defaultValue={attrs.value}
+              required={attrs.required}
+              disabled={attrs.disabled}
+            />
+          );
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <form action={flow.ui.action} method={flow.ui.method}>
+        {filterNodesByGroups({
+          nodes: flow.ui.nodes,
+          groups: ["default", "password"],
+        }).map((node, idx) => (
+          <div key={idx}> {mapUINode(node, idx)}</div>
+        ))}
+      </form>
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps<RegistrationProps> =
   async ({ req, query }) => {
     const flow = query?.flow as string | undefined;
-
-    console.log("#### flow", flow);
-    console.log("#### req.headers.cookie", req.headers.cookie);
 
     if (!isQuerySet(flow)) {
       const initFlowUrl = getUrlForFlow(basePath, "registration");
@@ -33,20 +87,17 @@ export const getServerSideProps: GetServerSideProps<RegistrationProps> =
         id: flow,
         cookie: req.headers.cookie,
       });
-      console.log("#### regFlow", regFlow);
 
-      // Add your server-side logic here
       return {
         props: {
           flow: regFlow.data,
         },
       };
     } catch (error) {
-      console.log("#### error", error);
-
       return {
-        props: {
-          error,
+        redirect: {
+          destination: "/registration",
+          permanent: false,
         },
       };
     }
