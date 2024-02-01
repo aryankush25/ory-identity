fbcbacb228f8166946a34ea1bda46fa0a0578e47
Aryan Agarwal <aryankush025@gmail.com>
2024-02-01 19:38:19 +0530

    Add ory.ts file with utility functions

---
+++ ory-ui-nextjs/src/services/ory.ts
@@ -0,0 +1,30 @@
+import { Configuration, FrontendApi, Identity } from "@ory/client";
+
+export const basePath = "http://127.0.0.1:4433";
+
+export const ory = new FrontendApi(
+  new Configuration({
+    basePath,
+    baseOptions: {
+      withCredentials: true,
+    },
+  })
+);
+
+// Returns either the email or the username depending on the user's Identity Schema
+export const getUserName = (identity: Identity) =>
+  identity.traits.email || identity.traits.username;
+
+export const removeTrailingSlash = (s: string) => s.replace(/\/$/, "");
+
+export const getUrlForFlow = (
+  base: string,
+  flow: string,
+  query?: URLSearchParams
+) =>
+  `${removeTrailingSlash(base)}/self-service/${flow}/browser${
+    query ? `?${query.toString()}` : ""
+  }`;
+
+export const isQuerySet = (x: any): x is string =>
+  typeof x === "string" && x.length > 0;
