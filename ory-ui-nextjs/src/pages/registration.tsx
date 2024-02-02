13cb767ec9e050dbc966ea21d1125ffdcb8debe4
Aryan Agarwal <aryankush025@gmail.com>
2024-02-02 14:11:26 +0530

    Update registration page UI

--- ory-ui-nextjs/src/pages/registration.tsx
+++ ory-ui-nextjs/src/pages/registration.tsx
@@ -1,19 +1,19 @@
 import { GetServerSideProps } from "next";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import {
   basePathBrowser,
   getUrlForFlow,
   isQuerySet,
   ory,
 } from "@/services/ory";
-import { UiNode, UiNodeInputAttributes } from "@ory/client";
+import { RegistrationFlow, UiNode, UiNodeInputAttributes } from "@ory/client";
 import {
   filterNodesByGroups,
   isUiNodeInputAttributes,
 } from "@ory/integrations/ui";
 
 interface RegistrationProps {
-  flow?: any;
+  flow: RegistrationFlow;
 }
 
 const Registration = ({ flow }: RegistrationProps) => {
@@ -27,21 +27,21 @@ const Registration = ({ flow }: RegistrationProps) => {
         case "submit":
           return (
             <button
-              className="w-full px-3 py-2 mb-2 text-sm font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 mb-2 text-sm font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-700 focus:outline-none focus:shadow-outline dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:shadow-outline"
               title="Submit"
               type={attrs.type as "submit" | "reset" | "button" | undefined}
               name={attrs.name}
               value={attrs.value}
             >
-              Submit
+              {node.meta.label?.text}
             </button>
           );
         default:
           return (
             <input
-              className="w-full px-3 py-2 mb-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 mb-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:focus:shadow-outline"
               title="Input field"
-              placeholder={"Enter value for " + attrs.name}
+              placeholder={"Enter " + node.meta.label?.text}
               name={attrs.name}
               type={attrs.type}
               autoComplete={
@@ -58,16 +58,30 @@ const Registration = ({ flow }: RegistrationProps) => {
     }
   }, []);
 
+  const nodes = useMemo(
+    () =>
+      filterNodesByGroups({
+        nodes: flow.ui.nodes,
+        groups: ["default", "password"],
+      }),
+    [flow.ui.nodes]
+  );
+
   return (
-    <div>
-      <form action={flow.ui.action} method={flow.ui.method}>
-        {filterNodesByGroups({
-          nodes: flow.ui.nodes,
-          groups: ["default", "password"],
-        }).map((node, idx) => (
-          <div key={idx}> {mapUINode(node, idx)}</div>
-        ))}
-      </form>
+    <div className="flex justify-center items-center h-screen dark:bg-gray-900">
+      <div className="w-full max-w-md">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 dark:bg-gray-800"
+          action={flow.ui.action}
+          method={flow.ui.method}
+        >
+          {nodes.map((node, idx) => (
+            <div key={idx} className="mb-4">
+              {mapUINode(node, idx)}
+            </div>
+          ))}
+        </form>
+      </div>
     </div>
   );
 };
