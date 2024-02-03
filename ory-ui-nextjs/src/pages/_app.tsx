0bcea57ea79fb910761ec45f1759f79db7051765
Aryan Agarwal <aryankush025@gmail.com>
2024-02-03 16:08:34 +0530

    Add Inter font and update global font-family

--- ory-ui-nextjs/src/pages/_app.tsx
+++ ory-ui-nextjs/src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { Fragment } from "react";
+
+const myFont = Inter({
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <style jsx global>{`
+        html {
+          font-family: ${myFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
--- ory-ui-nextjs/tailwind.config.ts
+++ ory-ui-nextjs/tailwind.config.ts
@@ -1,20 +1,23 @@
 import type { Config } from "tailwindcss";
 
+import defaultTheme from "tailwindcss/defaultTheme";
+
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
     },
+    fontWeight: {
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
