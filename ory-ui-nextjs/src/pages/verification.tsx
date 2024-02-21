0d095d8ad3d1ad01ce19e51f7e953bb09de94d94
Aryan Agarwal <aryankush025@gmail.com>
2024-02-21 21:30:00 +0530

    Verification page message handling added

--- ory-ui-nextjs/src/pages/verification.tsx
+++ ory-ui-nextjs/src/pages/verification.tsx
@@ -5,67 +5,107 @@ import {
   isQuerySet,
   ory,
 } from "@/services/ory";
-import { VerificationFlow } from "@ory/client";
+import { UiText, VerificationFlow } from "@ory/client";
 import { handleGetFlowError } from "@/services/ory/error";
 import { UserAuthCard } from "@ory/elements";
 
+const flowType = "verification";
+
 interface VerificationProps {
   flow: VerificationFlow;
+  signupURL: string;
 }
 
-const Verification = ({ flow }: VerificationProps) => {
+const Verification = ({ flow, signupURL }: VerificationProps) => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <UserAuthCard
         flow={flow}
-        flowType="verification"
+        flowType={flowType}
         additionalProps={{
-          signupURL: "/registration",
+          signupURL: signupURL,
         }}
       />
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<VerificationProps> =
-  async ({ req, query }) => {
-    try {
-      const flow = query?.flow as string | undefined;
+export const getServerSideProps: GetServerSideProps<
+  VerificationProps
+> = async ({ req, query }) => {
+  const { flow, return_to = "", message } = query;
 
-      if (!isQuerySet(flow)) {
-        const initFlowUrl = getUrlForFlow(basePathBrowser, "verification");
+  if (!isQuerySet(flow)) {
+    const initFlowUrl = getUrlForFlow(
+      basePathBrowser,
+      flowType,
+      new URLSearchParams({
+        return_to: return_to.toString(),
+        ...(message ? { message: message.toString() } : {}),
+      }),
+    );
 
-        return {
-          redirect: {
-            destination: initFlowUrl,
-            permanent: false,
-          },
-        };
-      }
+    return {
+      redirect: {
+        destination: initFlowUrl,
+        permanent: false,
+      },
+    };
+  }
 
-      const verificationFlow = await ory.getVerificationFlow({
+  try {
+    const verificationFlow = (
+      await ory.getVerificationFlow({
         id: flow,
         cookie: req.headers.cookie,
-      });
+      })
+    ).data;
 
-      return {
-        props: {
-          flow: verificationFlow.data,
-        },
-      };
-    } catch (error) {
-      const errorData = handleGetFlowError("verification")(error);
+    const initRegistrationUrl = getUrlForFlow(
+      basePathBrowser,
+      "registration",
+      new URLSearchParams({
+        return_to:
+          (return_to && return_to.toString()) ||
+          verificationFlow.return_to ||
+          "",
+      }),
+    );
 
-      return {
-        redirect: {
-          destination: errorData
-            ? errorData.redirectTo
-            : "/error?flow=verification&error=" +
-              encodeURIComponent(JSON.stringify(error)),
-          permanent: false,
-        },
-      };
+    const message = new URL(
+      verificationFlow.request_url || "",
+    ).searchParams.get("message");
+
+    // check for custom messages in the request_url
+    if (isQuerySet(message)) {
+      const m: UiText[] = JSON.parse(message || "");
+
+      // add them to the flow data so they can be rendered by the UI
+      verificationFlow.ui.messages = [
+        ...(verificationFlow.ui.messages || []),
+        ...m,
+      ];
     }
-  };
+
+    return {
+      props: {
+        flow: verificationFlow,
+        signupURL: initRegistrationUrl,
+      },
+    };
+  } catch (error) {
+    const errorData = handleGetFlowError(flowType)(error);
+
+    return {
+      redirect: {
+        destination: errorData
+          ? errorData.redirectTo
+          : `/error?flow=${flowType}&error=` +
+            encodeURIComponent(JSON.stringify(error)),
+        permanent: false,
+      },
+    };
+  }
+};
 
 export default Verification;
