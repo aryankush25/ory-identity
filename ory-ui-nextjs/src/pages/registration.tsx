4e1453d31d13a212be79d551e85b1eef424010a6
Aryan Agarwal <aryankush025@gmail.com>
2024-02-21 21:48:20 +0530

    Registration page revamp

--- ory-ui-nextjs/src/pages/registration.tsx
+++ ory-ui-nextjs/src/pages/registration.tsx
@@ -9,18 +9,21 @@ import { RegistrationFlow } from "@ory/client";
 import { handleGetFlowError } from "@/services/ory/error";
 import { UserAuthCard } from "@ory/elements";
 
+const flowType = "registration";
+
 interface RegistrationProps {
   flow: RegistrationFlow;
+  loginURL: string;
 }
 
-const Registration = ({ flow }: RegistrationProps) => {
+const Registration = ({ flow, loginURL }: RegistrationProps) => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <UserAuthCard
         flow={flow}
-        flowType="registration"
+        flowType={flowType}
         additionalProps={{
-          loginURL: "/login",
+          loginURL: loginURL,
         }}
         includeScripts
       />
@@ -28,45 +31,85 @@ const Registration = ({ flow }: RegistrationProps) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps<RegistrationProps> =
-  async ({ req, query }) => {
-    try {
-      const flow = query?.flow as string | undefined;
+export const getServerSideProps: GetServerSideProps<
+  RegistrationProps
+> = async ({ req, query }) => {
+  const {
+    flow,
+    return_to,
+    after_verification_return_to,
+    login_challenge,
+    organization,
+  } = query;
 
-      if (!isQuerySet(flow)) {
-        const initFlowUrl = getUrlForFlow(basePathBrowser, "registration");
+  const initFlowQuery = new URLSearchParams({
+    ...(return_to && { return_to: return_to.toString() }),
+    ...(organization && { organization: organization.toString() }),
+    ...(after_verification_return_to && {
+      after_verification_return_to: after_verification_return_to.toString(),
+    }),
+  });
 
-        return {
-          redirect: {
-            destination: initFlowUrl,
-            permanent: false,
-          },
-        };
-      }
+  if (isQuerySet(login_challenge)) {
+    initFlowQuery.append("login_challenge", login_challenge);
+  }
 
-      const regFlow = await ory.getRegistrationFlow({
+  if (!isQuerySet(flow)) {
+    const initFlowUrl = getUrlForFlow(basePathBrowser, flowType, initFlowQuery);
+
+    return {
+      redirect: {
+        destination: initFlowUrl,
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const registrationFlow = (
+      await ory.getRegistrationFlow({
         id: flow,
         cookie: req.headers.cookie,
-      });
+      })
+    ).data;
 
-      return {
-        props: {
-          flow: regFlow.data,
-        },
-      };
-    } catch (error) {
-      const errorData = handleGetFlowError("registration")(error);
+    const initLoginQuery = new URLSearchParams({
+      return_to:
+        (return_to && return_to.toString()) || registrationFlow.return_to || "",
+    });
 
-      return {
-        redirect: {
-          destination: errorData
-            ? errorData.redirectTo
-            : "/error?flow=registration&error=" +
-              encodeURIComponent(JSON.stringify(error)),
-          permanent: false,
-        },
-      };
+    if (registrationFlow.oauth2_login_request?.challenge) {
+      initLoginQuery.set(
+        "login_challenge",
+        registrationFlow.oauth2_login_request.challenge,
+      );
     }
-  };
+
+    const initLoginUrl = getUrlForFlow(
+      basePathBrowser,
+      "login",
+      initLoginQuery,
+    );
+
+    return {
+      props: {
+        flow: registrationFlow,
+        loginURL: initLoginUrl,
+      },
+    };
+  } catch (error) {
+    const errorData = handleGetFlowError(flowType)(error);
+
+    return {
+      redirect: {
+        destination: errorData
+          ? errorData.redirectTo
+          : `/error?flow=${flowType}&error=` +
+            encodeURIComponent(JSON.stringify(error)),
+        permanent: false,
+      },
+    };
+  }
+};
 
 export default Registration;
