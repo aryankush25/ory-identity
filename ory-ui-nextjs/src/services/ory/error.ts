7a7b382b854162ea555209183176bb5b11a21b34
Aryan Agarwal <aryankush025@gmail.com>
2024-02-22 14:17:05 +0530

    Commented out the url field in handleError function

--- ory-ui-nextjs/src/services/ory/error.ts
+++ ory-ui-nextjs/src/services/ory/error.ts
@@ -148,7 +148,7 @@ export const handleError = (error: any): { redirectTo: string } => {
         data: error.response?.data || error,
         status: error.response?.status,
         statusText: error.response?.statusText,
-        url: window.location.href,
+        // url: window.location.href,
       };
 
       return {
