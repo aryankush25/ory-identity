51139319174625fd1457e797d61fe6501d7dea46
Aryan Agarwal <aryankush025@gmail.com>
2024-02-23 19:17:16 +0530

    Update apiBaseOauth2UrlInternal in envConfig.ts

--- ory-ui-nextjs/src/utils/envConfig.ts
+++ ory-ui-nextjs/src/utils/envConfig.ts
@@ -3,7 +3,8 @@ export const oryConfig = {
     process.env.NEXT_PUBLIC_KRATOS_BROWSER_URL || "http://127.0.0.1:4433/",
   apiBaseFrontendUrlInternal:
     process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL || "http://kratos:4433/",
-  apiBaseOauth2UrlInternal: process.env.HYDRA_ADMIN_URL || "http://hydra:4445/",
+  apiBaseOauth2UrlInternal:
+    process.env.NEXT_PUBLIC_HYDRA_ADMIN_URL || "http://hydra:4445/",
 
   mockTlsTermination: process.env.MOCK_TLS_TERMINATION,
   trustedClientIds: process.env.TRUSTED_CLIENT_IDS,
